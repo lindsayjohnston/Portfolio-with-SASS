b2b226093c079f2f0bacc5434bb6fc07c3a45aaf
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-10-30 11:24:44 -0500

    Basic layout for navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,6 +3,12 @@ import styles from './Navbar.module.scss';
 
 const navbar = (props) =>(
     <div className={styles.Navbar}>
+        <ul>
+            <li><a href="#Projects">Projects</a></li>
+            <li><a href="#AboutMe">About Me</a></li>
+            <li><a href="#Contact">Contact</a></li>
+            
+        </ul>
        
     </div>
 );
